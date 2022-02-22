82fc460292a1b3e95dcbe7baf598e273ac8db951
NidhiB <100119660+Nidhib03@users.noreply.github.com>
2022-02-22 16:59:02 +0530

    Update csv1.py

--- csv1.py
+++ csv1.py
@@ -17,30 +17,30 @@ with open(filename, 'w') as csvfile:
 
 # # OR
 
-# # importing the csv module
-# import csv
+# importing the csv module
+import csv
 	
-# # my data rows as dictionary objects
-# mydict =[{'branch': 'COE', 'cgpa': '9.0', 'name': 'Nikhil', 'year': '2'},
-# 		{'branch': 'COE', 'cgpa': '9.1', 'name': 'Sanchit', 'year': '2'},
-# 		{'branch': 'IT', 'cgpa': '9.3', 'name': 'Aditya', 'year': '2'},
-# 		{'branch': 'SE', 'cgpa': '9.5', 'name': 'Sagar', 'year': '1'},
-# 		{'branch': 'MCE', 'cgpa': '7.8', 'name': 'Prateek', 'year': '3'},
-# 		{'branch': 'EP', 'cgpa': '9.1', 'name': 'Sahil', 'year': '2'}]
+# my data rows as dictionary objects
+mydict =[{'branch': 'COE', 'cgpa': '9.0', 'name': 'n1', 'year': '2'},
+		{'branch': 'COE', 'cgpa': '9.1', 'name': 'n2', 'year': '2'},
+		{'branch': 'IT', 'cgpa': '9.3', 'name': 'n3', 'year': '2'},
+		{'branch': 'SE', 'cgpa': '9.5', 'name': 'n4', 'year': '1'},
+		{'branch': 'MCE', 'cgpa': '7.8', 'name': 'n5', 'year': '3'},
+		{'branch': 'EP', 'cgpa': '9.1', 'name': 'n6', 'year': '2'}]
 	
-# # field names
-# fields = ['name', 'branch', 'year', 'cgpa']
+# field names
+fields = ['name', 'branch', 'year', 'cgpa']
 	
-# # name of csv file
-# filename = "university_records.csv"
+# name of csv file
+filename = "university_records.csv"
 	
-# # writing to csv file
-# with open(filename, 'w') as csvfile:
-# 	# creating a csv dict writer object
-# 	writer = csv.DictWriter(csvfile, fieldnames = fields)
+# writing to csv file
+with open(filename, 'w') as csvfile:
+	# creating a csv dict writer object
+	writer = csv.DictWriter(csvfile, fieldnames = fields)
 		
-# 	# writing headers (field names)
-# 	writer.writeheader()
+	# writing headers (field names)
+	writer.writeheader()
 		
-# 	# writing data rows
-# 	writer.writerows(mydict)
+	# writing data rows
+	writer.writerows(mydict)
